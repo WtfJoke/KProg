e9d263ece6b093298b9c5d0ab3f9130151f0b444
Manuel <wtfjoke@livenet.ch>
2016-03-17 10:12:46 +0100

    init project with empty classes

---
+++ KProg/src/com/foo/first/FastPipe.java
@@ -0,0 +1,10 @@
+package com.foo.first;
+
+public class FastPipe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ KProg/src/com/foo/first/FuenfSpaltenPrimzahlen.java
@@ -0,0 +1,5 @@
+package com.foo.first;
+
+public class FuenfSpaltenPrimzahlen {
+
+}
