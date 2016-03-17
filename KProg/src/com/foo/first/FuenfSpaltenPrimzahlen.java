35e34ed046dbc89c8dfa11d3adb3c4b57b7f258b
Manuel <wtfjoke@livenet.ch>
2016-03-17 11:27:01 +0100

    add main method
    five pipes which gets filled

--- KProg/src/com/foo/first/FuenfSpaltenPrimzahlen.java
+++ KProg/src/com/foo/first/FuenfSpaltenPrimzahlen.java
@@ -1,5 +1,43 @@
 package com.foo.first;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FuenfSpaltenPrimzahlen {
+
+	private static final int PRIMEMAX = 5000;
+
+	public static void main(String[] args) {
+		List<Primer> primers = new ArrayList<>();
+		primers.add(new Primer(2, 0));
+		primers.add(new Primer(2, 2));
+		primers.add(new Primer(2, 4));
+		primers.add(new Primer(2, 6));
+		primers.add(new Primer(2, 8));
+
+		sendAll(primers);
+		stopAll(primers);
+	}
+
+	static void sendAll(List<Primer> primers) {
+		for (Primer primer : primers) {
+			send(primer);
+		}
+	}
+
+	static void stopAll(List<Primer> primers) {
+		for (Primer primer : primers) {
+			stop(primer);
+		}
+	}
+
+	static void send(Primer primer) {
+		for (int i = 3; i <= PRIMEMAX; primer.send(i++)) {
+		}
+	}
+
+	static void stop(Primer primer) {
+		primer.send(0);
+	}
 
 }
