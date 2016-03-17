b62348bbc91bdcbbcfcb993e2ceef288b3279246
Manuel <wtfjoke@livenet.ch>
2016-03-17 14:02:25 +0100

    added simple element

---
+++ KProg/src/com/foo/first/Element.java
@@ -0,0 +1,57 @@
+package com.foo.first;
+
+public class Element extends Thread {
+
+	private int i;
+	private Element next;
+	private int buffer = -1;
+
+	public Element(int p) {
+		this.i = p;
+		this.start();
+	}
+
+	@Override
+	public void run() {
+		System.out.println(i);
+		while (true) {
+			int n = receive();
+			if (n == 0) {
+				if (next != null) {
+					next.send(n);
+				}
+				break;
+			} else {
+				if (next != null) {
+					next.send(n);
+				} else {
+					next = new Element(n);
+				}
+			}
+		}
+	}
+
+	synchronized int receive() {
+		int result = 0;
+		try {
+			while ((result = buffer) < 0) {
+				wait();
+			}
+			buffer = -1;
+			notify();
+		} catch (InterruptedException e) {
+		}
+		return result;
+	}
+
+	synchronized void send(int i) {
+		try {
+			while (buffer >= 0) {
+				wait();
+			}
+			buffer = i;
+			notify();
+		} catch (InterruptedException e) {
+		}
+	}
+}
