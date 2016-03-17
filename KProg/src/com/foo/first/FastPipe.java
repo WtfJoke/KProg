751da50f6098ef4c9af6bed9a24fa9a3a1dc8416
Manuel <wtfjoke@livenet.ch>
2016-03-17 14:02:37 +0100

    add fastpipe main

--- KProg/src/com/foo/first/FastPipe.java
+++ KProg/src/com/foo/first/FastPipe.java
@@ -2,9 +2,13 @@ package com.foo.first;
 
 public class FastPipe {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private static int MAX = 10000;
 
+	public static void main(String[] args) {
+		Element element = new Element(1);
+		for (int i = 1; i <= MAX; element.send(i++)) {
+		}
+		element.send(0);
 	}
 
 }
