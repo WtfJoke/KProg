b0f2f6bcbedf599f3e4aea39d90e208b95e83543
Manuel <wtfjoke@livenet.ch>
2016-03-17 14:27:32 +0100

    increase max

--- KProg/src/com/foo/first/FastPipe.java
+++ KProg/src/com/foo/first/FastPipe.java
@@ -2,7 +2,7 @@ package com.foo.first;
 
 public class FastPipe {
 
-	private static int MAX = 10000;
+	private static int MAX = 100000;
 
 	public static void main(String[] args) {
 		Element element = new Element(1);
