192e3a5df2571b4c2236e946008b9595f3b5c9ff
Manuel <wtfjoke@livenet.ch>
2016-03-24 12:35:46 +0100

    cycle colors

--- KProg/src/com/foo/second/Regenbogen.java
+++ KProg/src/com/foo/second/Regenbogen.java
@@ -1,17 +1,33 @@
 package com.foo.second;
 
+import java.awt.Button;
 import java.awt.FlowLayout;
 import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Timer;
 
 public class Regenbogen extends Frame {
 
 	private static final long serialVersionUID = 4860837199461628871L;
+	private Button newWindowRainbow = new Button("New Window");
 
 	public Regenbogen() {
 		addWindowListener(new WindowQuitter());
 		setSize(400, 300);
 		setVisible(true);
 		setLayout(new FlowLayout());
+		add(newWindowRainbow);
+		newWindowRainbow.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				new Regenbogen();
+			}
+		});
+		Timer timer = new Timer(750, new SwitchColorActionListener(this));
+		timer.start();
 	}
 
 	public static void main(String[] args) {
