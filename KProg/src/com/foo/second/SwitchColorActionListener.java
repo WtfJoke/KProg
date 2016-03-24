c46a5143871e6d1056528db46cb1cb5ff42eb168
Manuel <wtfjoke@livenet.ch>
2016-03-24 12:37:34 +0100

    undo extract switch color logic to own class

--- KProg/src/com/foo/second/SwitchColorActionListener.java
+++ KProg/src/com/foo/second/SwitchColorActionListener.java
@@ -1,19 +1,32 @@
 package com.foo.second;
 
+import java.awt.Color;
 import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
 
 public class SwitchColorActionListener implements ActionListener {
 
-	private SwitchColor switchColor;
+	private int currentColorIndex;
+	private final List<Color> correspondingColors;
+	private final Frame correspondingFrame;
 
 	public SwitchColorActionListener(Frame correspondingFrame) {
-		switchColor = new SwitchColor(correspondingFrame);
+		this.correspondingFrame = correspondingFrame;
+		this.correspondingColors = Colors.getCorrespondingColors();
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		switchColor.switchIt();
+		correspondingFrame.setBackground(correspondingColors.get(getIndex()));
 	}
+
+	private int getIndex() {
+		if (currentColorIndex++ >= correspondingColors.size() - 1) {
+			currentColorIndex = 0;
+		}
+		return currentColorIndex;
+	}
+
 }
