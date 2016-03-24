b9f86e4a7b2c675b2ba1367e567b19cbb27e9579
Manuel <wtfjoke@livenet.ch>
2016-03-24 13:18:08 +0100

    added lockcode

--- KProg/src/com/foo/second/Schloss.java
+++ KProg/src/com/foo/second/Schloss.java
@@ -3,6 +3,8 @@ package com.foo.second;
 import java.awt.Button;
 import java.awt.FlowLayout;
 import java.awt.Frame;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Schloss extends Frame {
 	private static final long serialVersionUID = 2624446559320231971L;
@@ -16,18 +18,37 @@ public class Schloss extends Frame {
 	private Button button7 = new Button("7");
 	private Button button8 = new Button("8");
 	private Button button9 = new Button("9");
+	private List<Integer> unlockCode;
 
 	public Schloss() {
 		addWindowListener(new WindowQuitter());
-		setSize(400, 300);
+		setSize(1000, 400);
 		setVisible(true);
 		setLayout(new FlowLayout());
+		createUnlockCode();
 		addButtons(button0, button1, button2, button3, button4, button5, button6, button7, button8, button9);
 	}
 
+	private void createUnlockCode() {
+		unlockCode = new ArrayList<>();
+		unlockCode.add(Integer.valueOf(2));
+		unlockCode.add(Integer.valueOf(3));
+		unlockCode.add(Integer.valueOf(0));
+		unlockCode.add(Integer.valueOf(3));
+		unlockCode.add(Integer.valueOf(6));
+		unlockCode.add(Integer.valueOf(0));
+	}
+
+	public static void main(String[] args) {
+		new Schloss();
+	}
+
 	private void addButtons(Button... buttons) {
+		UnlockActionListener unlockActionListener = new UnlockActionListener(this, unlockCode);
 		for (Button button : buttons) {
 			add(button);
+			button.setActionCommand(button.getLabel());
+			button.addActionListener(unlockActionListener);
 		}
 	}
 }
