38fe7578e9a75f594bb0ee340d19afd5fbc9c14a
Manuel <wtfjoke@livenet.ch>
2016-03-31 09:56:53 +0200

    implement dynamic button creation

--- KProg/src/com/foo/second/Schloss.java
+++ KProg/src/com/foo/second/Schloss.java
@@ -16,16 +16,7 @@ import java.util.List;
  */
 public class Schloss extends Frame {
 	private static final long serialVersionUID = 2624446559320231971L;
-	private Button button0 = new Button("0");
-	private Button button1 = new Button("1");
-	private Button button2 = new Button("2");
-	private Button button3 = new Button("3");
-	private Button button4 = new Button("4");
-	private Button button5 = new Button("5");
-	private Button button6 = new Button("6");
-	private Button button7 = new Button("7");
-	private Button button8 = new Button("8");
-	private Button button9 = new Button("9");
+	private static final int AMOUNTOFBUTTONS = 10;
 	private List<Integer> unlockCode;
 
 	public Schloss() {
@@ -34,7 +25,7 @@ public class Schloss extends Frame {
 		setVisible(true);
 		setLayout(new FlowLayout());
 		createUnlockCode();
-		addButtons(button0, button1, button2, button3, button4, button5, button6, button7, button8, button9);
+		addButtons();
 	}
 
 	private void createUnlockCode() {
@@ -51,9 +42,10 @@ public class Schloss extends Frame {
 		new Schloss();
 	}
 
-	private void addButtons(Button... buttons) {
+	private void addButtons() {
 		UnlockActionListener unlockActionListener = new UnlockActionListener(this, unlockCode);
-		for (Button button : buttons) {
+		for (int i = 0; i < AMOUNTOFBUTTONS; i++) {
+			Button button = new Button(String.valueOf(i));
 			add(button);
 			button.setActionCommand(button.getLabel());
 			button.addActionListener(unlockActionListener);
