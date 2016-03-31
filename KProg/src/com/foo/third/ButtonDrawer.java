0db86706ecaab9c8b8292cc3221f2dd77aaa1fef
Manuel <wtfjoke@livenet.ch>
2016-03-31 14:35:53 +0200

    remove useless code / change hack a bit

--- KProg/src/com/foo/third/ButtonDrawer.java
+++ KProg/src/com/foo/third/ButtonDrawer.java
@@ -10,8 +10,6 @@ import javax.swing.SwingUtilities;
 
 public abstract class ButtonDrawer {
 
-	private static final int CENTER_DOWN_EMPTY_GRID_LOCATION = 7;
-	private static final int CENTER_UP_EMPTY_GRID_LOCATION = 4;
 	private static JPanel eastButtonPanel;
 	private static JPanel westButtonPanel;
 	private static JPanel northButtonPanel;
@@ -21,8 +19,7 @@ public abstract class ButtonDrawer {
 		eastButtonPanel = new JPanel();
 		eastButtonPanel.setLayout(new BoxLayout(eastButtonPanel, BoxLayout.Y_AXIS));
 		westButtonPanel = new JPanel();
-		westButtonPanel.setLayout(new BoxLayout(westButtonPanel, BoxLayout.PAGE_AXIS));
-		westButtonPanel.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+		westButtonPanel.setLayout(new BoxLayout(westButtonPanel, BoxLayout.Y_AXIS));
 
 		northButtonPanel = new JPanel();
 		northButtonPanel.setLayout(new BoxLayout(northButtonPanel, BoxLayout.X_AXIS));
@@ -40,31 +37,27 @@ public abstract class ButtonDrawer {
 	}
 
 	static void draw(List<JButton> buttons) {
+		JButton postponedWestButton = null;
 		for (int i = 0; i < buttons.size(); i++) {
 			JButton button = buttons.get(i);
 			if (i < 3) {
 				northButtonPanel.add(button);
-				// } else if (i == 8 || i == 9) {
-				// westButtonPanel.add(button);
 			} else if (i == 3 || i == 4) {
 				eastButtonPanel.add(button);
+			} else if (i == 8) {
+				postponedWestButton = button;
+			} else if (i == 9) {
+				westButtonPanel.add(button);
 			} else {
 				southButtonPanel.add(button);
 			}
 		}
 
-		westButtonPanel.add(buttons.get(9));
-		westButtonPanel.add(buttons.get(8));
-		// westButtonPanel.getComponent(9);
-
-		// addEmptyCellsInCenter(buttonPanel);
+		if (postponedWestButton != null) {
+			westButtonPanel.add(postponedWestButton);
+		}
 
 	}
-
-	// private static void addEmptyCellsInCenter(Container contentPane) {
-	// contentPane.add(new JLabel(""), null, CENTER_UP_EMPTY_GRID_LOCATION);
-	// contentPane.add(new JLabel(""), null, CENTER_DOWN_EMPTY_GRID_LOCATION);
-	// }
 
 	static JPanel getEastButtonPanel() {
 		return eastButtonPanel;
